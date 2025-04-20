871b875daa24901cdabadc09336d06df59269ce3
VISHAL DHANGAR <vishaldhangarmca21@gmail.com>
2025-04-20 19:05:09 +0530

    Added Tailwind CSS with Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './components/redux/store.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
